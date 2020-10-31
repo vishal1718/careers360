bcea8a2e51eaa15e785e9a7e47a1ce1c880855a8
vishal1718 <73627475+vishal1718@users.noreply.github.com>
2020-10-31 21:04:29 +0530

    Add files via upload

---
+++ javas.js
@@ -0,0 +1,17 @@
+function d(vis){
+ document.getElementById('sol').value+=vis;
+
+}
+function result(){
+     var value1=document.getElementById('sol').value;
+      res=eval(value1);
+     document.getElementById('sol').value=res;
+
+}
+
+
+function clr(){
+ document.getElementById('sol').value="";
+
+}
+s
